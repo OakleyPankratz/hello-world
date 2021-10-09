74a2ac055fb04e2a1e19028f2ff30aae4677d365
Oakley <oakley@dhcp-206-12-52-99.eduroam.wireless.ubc.ca>
2021-10-09 14:44:16 -0700

    add scanner input

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("byeWorld");
+		Scanner in = new Scanner(System.in);
+		int num = in.nextInt();
+		System.out.println("You entered: " + num);
+		in.close(); 
 	}	
 }
